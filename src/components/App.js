a726c3a0405d82f9d17147950013b0f056c7413d
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-21 12:43:14 +0100

    OTHER Setup eslint and codeclimate

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <h2>Hello World!</h2>
-      </div>
-    )
-  }
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello World!</h2>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
