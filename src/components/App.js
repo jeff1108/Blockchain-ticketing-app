f41d2264fbb7c5b3f2ae910d5fcd090a56624fa3
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-22 13:51:12 +0100

    OTHER Work in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Hello World!</h2>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    fetch('https://localhost:9292', {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         concert_name: 'Conert_name',
+         price: 16,
+         description: 'hello',
+         sender_id: 1234,
+         sender_address: 'seller@gmail.com',
+         receiver_address: 2345,
+         receiver_address: 'receiver_address'
+       })
+    })
+  }
+  render() {
+      return (
+        <div>
+          <div>
+            <h1>Ticket Master</h1>
+          </div>
+          <hr />
+          <div>
+              <h4>Concert Name: Justin Bieber</h4>
+              <h4>Description: Concert in London</h4>
+              <h4>Price: Adult £15 </h4>
+          </div>
+          <div align='right'>
+              <button onClick={this.handleClick}>Submit</button>
+          </div>
+          <hr />
+          <div>
+              <h4>Concert Name: Justin Bieber</h4>
+              <h4>Description: Concert in London</h4>
+              <h4>Price: Adult £15 </h4>
+          </div>
+          <div align='right'>
+              <button>Submit</button>
+          </div>
+          <hr />
+        </div>
+      );
+  }
 }
 
 export default App;
