83caf6ffa75111191c086af5b960a6f6d25a2c56
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-23 14:46:15 +0100

    OTHER Refactor the test and show the concert search text

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
+  state = {
       concert1: {
         name: "Bruno Mars",
         description: "Lanuch in Hong Kong 2018",
@@ -17,36 +14,14 @@ class App extends Component {
         price: "£100"
       }
     }
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-
-    fetch('/api/form-submit-url', {
-      method: 'POST',
-      body: data,
-    });
-  }
 
   render() {
     return (
-      <div>
-      <h2>ALL TICKETS</h2>
-      <form onSubmit={this.handleSubmit}>
-
-        <label htmlFor="concert_name">Concert:
-          <h4>{this.state.concert1.name}</h4>
-        </label>
-        <label htmlFor="concert_description">Description:
-          <h4>{this.state.concert1.description}</h4>
-        </label>
-        <label htmlFor="concert_price">Price:
-          <h4>{this.state.concert1.price}</h4>
-        </label>
-        <button>Buy!</button>
-      </form>
-      </div>
+      <div className='App'>
+        <header className='App-header'>
+          <h2 className='App-title'>Concert Search</h2>
+        </header>
+        </div>
     );
   }
 
@@ -56,10 +31,30 @@ class App extends Component {
 export default App;
 
 
-  //        concert_name: 'Conert_name',
-  //        price: 16,
-  //        description: 'hello',
-  //        sender_id: 1234,
-  //        sender_address: 'seller@gmail.com',
-  //        receiver_address: 2345,
-  //        receiver_address: 'receiver_address'
+// handleSubmit(event) {
+//   event.preventDefault();
+//   const data = new FormData(event.target);
+//
+//   fetch('/api/form-submit-url', {
+//     method: 'POST',
+//     body: data,
+//   });
+// }
+
+
+// <div>
+// <h2>ALL TICKETS</h2>
+// <form onSubmit={this.handleSubmit}>
+//
+//   <label htmlFor="concert_name">Concert:
+//     <h4>{this.state.concert1.name}</h4>
+//   </label>
+//   <label htmlFor="concert_description">Description:
+//     <h4>{this.state.concert1.description}</h4>
+//   </label>
+//   <label htmlFor="concert_price">Price:
+//     <h4>{this.state.concert1.price}</h4>
+//   </label>
+//   <button>Buy!</button>
+// </form>
+// </div>
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,24 +8,24 @@ describe('app component', () => {
   it('renders the App title', () => {
       const wrapper = shallow(<App />)
       const text = wrapper.find('h2').text()
-      expect(text).toEqual('ALL TICKETS');
+      expect(text).toEqual('Concert Search');
   });
 
-  it('show concert name', () => {
-    const wrapper = shallow(<App />)
-    const concert = wrapper.state().concert1.name
-    expect(concert).toEqual("Bruno Mars")
-  });
-
-  it('show concert name in the form', () => {
-    const wrapper = shallow(<App />)
-    const text = wrapper.find('h4').first().text()
-    expect(text).toEqual("Bruno Mars")
-  });
-
-  it('renders a buy button', () => {
-    const wrapper = shallow(<App />)
-    const button = wrapper.find('button').text()
-    expect(button).toEqual('Buy!')
-  });
+  // it('show concert name', () => {
+  //   const wrapper = shallow(<App />)
+  //   const concert = wrapper.state().concert1.name
+  //   expect(concert).toEqual("Bruno Mars")
+  // });
+  //
+  // it('show concert name in the form', () => {
+  //   const wrapper = shallow(<App />)
+  //   const text = wrapper.find('h4').first().text()
+  //   expect(text).toEqual("Bruno Mars")
+  // });
+  //
+  // it('renders a buy button', () => {
+  //   const wrapper = shallow(<App />)
+  //   const button = wrapper.find('button').text()
+  //   expect(button).toEqual('Buy!')
+  // });
 });
