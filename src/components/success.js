219093629a9ea14141c6920d2ec7fccffabe8d36
nabil far <nabil.far.contact@gmail.com>
2018-08-28 16:43:43 +0100

    add footer with songkick logo

---
+++ src/components/success.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Success extends Component {
+
+
+  render() {
+    return (
+      <div className='success'>
+        <p>Thank you</p>
+      </div>
+    );
+  };
+
+export default Success;
